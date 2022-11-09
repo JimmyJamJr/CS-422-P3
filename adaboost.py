08ade47e10a988f15b509622b515e63d7117ef65
Jimson Huang <jimsonjy@gmail.com>
2022-11-08 21:54:28 -0800

    More comments

--- adaboost.py
+++ adaboost.py
@@ -18,10 +18,12 @@ def adaboost_train(X, Y, max_iter):
         a = 0.5 * np.log((1 - err) / err)
         alpha.append(a)
 
+        # Create new data set, if the prediction doesn't match the label, duplicate the sample
+        # NOT SURE IF CORRECT
         new_X = []
         new_y = []
         for i in range(len(Y)):
-            if np.exp(-a) * Y[i] * predictions[i] <= 0:
+            if Y[i] * predictions[i] <= 0:
                 new_y += 2 * [Y[i]]
                 new_X += 2 * [X[i]]
             else:
@@ -39,18 +41,3 @@ def adaboost_test(X, Y, f, alpha):
         if np.sign(np.sum([alpha[j] * f[j].predict(np.array(X[i]).reshape(1, -1)) for j in range(len(f))])) * Y[i] > 0:
             correct += 1
     return correct / len(Y)
-
-
-# X = [[2,3], [2,2], [4,6], [4,3], [4,1], [5,7], [5,3], [6,5], [8,6], [8,2]]
-# Y=[1,1,1,-1,-1,1,-1,1,-1,-1]
-# # X = [[-2,-2],[-3,-2],[-2,-3],[-1,-1],[-1,0],[0,-1],[1,1],[1,0],[0,1],[2,2],[3,2],[2,3]]
-# # Y=[-1,-1,-1,1,1,1,-1,-1,-1,1,1,1]
-# f, alpha = adaboost_train(X,Y,5)
-# acc = adaboost_test(X,Y,f,alpha)
-# print("Accuracy:", acc)
-#
-# from sklearn.ensemble import AdaBoostClassifier
-# from sklearn.datasets import make_classification
-# clf = AdaBoostClassifier(n_estimators=5, random_state=0)
-# clf.fit(X, Y)
-# print(clf.score(X, Y))
